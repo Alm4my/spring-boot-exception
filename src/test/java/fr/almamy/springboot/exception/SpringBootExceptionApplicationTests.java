2ffdb561307d4d92f939a0bb235eaa781a123012
Alm4my <thenixadm@gmail.com>
2021-07-16 13:35:41 +0000

    Initial Commit

---
+++ src/test/java/fr/almamy/springboot/exception/SpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.almamy.springboot.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
